06b743663b9fefebf6cff8ef5ec202eff266f4da
virgilxw <reubenwxw@gmail.com>
2022-10-31 21:02:24 +0000

    1.1courseinfo done

---
+++ workfile/src/App.js
@@ -0,0 +1,19 @@
+const Hello = (props) => {
+    return (
+        <div>
+            <p>Hello {props.name}</p>
+        </div>
+    )
+}
+
+const App = () => {
+    return (
+        <div>
+            <h1>Greetings</h1>
+            <Hello name='George' />
+            <Hello name='Daisy' />
+        </div>
+    )
+}
+
+export default App
