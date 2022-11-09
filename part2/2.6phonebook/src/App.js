f934e6ae8882c86892e71f0ffc5e6248573e57f9
virgilxw <reubenwxw@gmail.com>
2022-11-09 15:24:30 +0000

    part 2 done

--- part2/2.6phonebook/src/App.js
+++ part2/2.6phonebook/src/App.js
@@ -1,5 +1,18 @@
 import { useState, useEffect } from 'react'
 import pbService from './services'
+import './style.css'
+
+const Notification = ({className, message}) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className= {`error ${className}`}>
+      {message}
+    </div>
+  )
+}
 
 const PersonForm = ({ addPerson, newName, handleNameChange, newNumber, handleNumberChange }) => {
   return (
@@ -62,6 +75,9 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const [goodErrorMessage, setGoodErrorMessage] = useState(null)
+  const [badErrorMessage, setBadErrorMessage] = useState(null)
+
   // Constructor
   const personConstructor = {
     name: newName,
@@ -85,6 +101,14 @@ const App = () => {
       if (window.confirm(`${newName} is already added to the phonebook. Replace the old number with the new one entered?`)) {
         pbService.update(match.id, personConstructor).then(retrunedBook => {
           pbService.getAll().then(input => setPersons(input))
+        }).catch((error) => {
+          console.log(error)
+          setBadErrorMessage(
+            `${newName} has been already removed from the server`
+          )
+          setTimeout(() => {
+            setBadErrorMessage(null)
+          }, 5000)
         })
         setNewName('')
         setNewNumber('')
@@ -95,6 +119,12 @@ const App = () => {
     else {
       pbService.create(personConstructor).then(retrunedBook => {
         pbService.getAll().then(input => setPersons(input))
+        setGoodErrorMessage(
+          `${newName} was added to server`
+        )
+        setTimeout(() => {
+          setGoodErrorMessage(null)
+        }, 5000)
       }).catch(error => {
         alert(
           `failed to push '${personConstructor.name}' to the server`
@@ -109,6 +139,8 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <PersonForm addPerson={addPerson} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange} />
+      <Notification className="good" message={goodErrorMessage} />
+      <Notification className="bad" message={badErrorMessage} />
       <h2>Numbers</h2>
       <Numbers persons={persons} setPersons={setPersons} />
     </div>
--- part2/2.6phonebook/src/services.js
+++ part2/2.6phonebook/src/services.js
@@ -7,7 +7,6 @@ const getAll = () => {
 }
 
 const create = newObject => {
-  console.log(`create newObject ${newObject}`)
   const request = axios.post(baseUrl, newObject)
   return request.then(response => response.data)
 }
